5dabd0aa8796a53649e42d11ca184f8180230968
Jasun-Maharjan <jsnmhrjn@gmail.com>
2026-07-17 08:28:55 +0545

    blog urls debug

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+def blog_list(request):
+    page = get_object_or_404(Blog)
+    return render(request, 'extending/blogs.html',{'page':page})
