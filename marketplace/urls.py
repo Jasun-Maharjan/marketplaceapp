3903b1c5a6675a35a9cc7b163e8b8d8bd5093fdd
Jasun-Maharjan <jsnmhrjn@gmail.com>
2026-07-24 08:11:46 +0545

    dynamic image handling

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('',views.home, name = 'home'),
     path('pages/', include('pages.urls')),
     path('blogs/',include('blogs.urls'))
-] + static(settings.MEDIA_URL), document_root = settings.MEDIA_ROOT
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
