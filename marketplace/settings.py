f4f4e0bf5427c181ef93fb20e21c998d0f7c5cad
Jasun-Maharjan <jsnmhrjn@gmail.com>
2026-07-24 07:38:48 +0545

    jazzmin setup

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,5 @@ DATABASES = {
         'NAME': BASE_DIR/'db.sqlite3',
     }
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from . models import Category, Product
+from django.utils.html import format_html
 # Register your models here.
 
 class productAdmin(admin.ModelAdmin):
     list_display = ('id','name','category','price','stock','status')
+    def image_preview(self, obj):
+        if obj.project_image:
+            return format_html('<img src="{}" width = "80" height = "80" style="object-fit:cover;"/>',obj.product_image.url)
+        return "No image"
+    image_preview.short_description = "Image"
 
 admin.site.register(Product,productAdmin)
 
